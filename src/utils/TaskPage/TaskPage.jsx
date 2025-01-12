02821b1952d13ef692b5e68851f55fba106ae732
Asirwad <asirwadsali@gmail.com>
2025-01-12 18:02:47 +0530

    add updateGigStatus

--- src/utils/TaskPage/TaskPage.jsx
+++ src/utils/TaskPage/TaskPage.jsx
@@ -1,18 +1,46 @@
 import {React} from 'react'
 import "./Taskpage.css"
 import {Button} from '../../ui/button'
+import axios from 'axios';
 const TaskPage = ({onViewButtonClick, job}) => {
  
-    const handleApproveButtonClick=()=>{
-    alert("Task approved!")
+  const updateGigStatus = async (status) => {
+    try {
+      const response = await axios.patch('http://localhost:8089/api/v1/update_gig', {
+        "gig_id": job.id,
+        "status": status
+      });
+      return response;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleApproveButtonClick=()=>{
+    const result = updateGigStatus("approved");
+    if(result){
+      alert("Task approved!")
+    }else{
+      alert("Task approval failed!")
+    }
   };
 
   const handlePauseButtonClick=()=>{
-    alert("Task paused!")
+    const result = updateGigStatus("paused");
+    if(result){
+      alert("Task paused!")
+    }else{
+      alert("Task pause failed!")
+    }
   };
 
   const handleRevokeButtonClick=()=>{
-    alert("Task revoked!")
+    const result = updateGigStatus("revoked");
+    if(result){
+      alert("Task revoked!")
+    }else{
+      alert("Task revokation failed!")
+    }
   };
  
   return (
@@ -23,10 +51,37 @@ const TaskPage = ({onViewButtonClick, job}) => {
           <div><p>{job.description}</p>
           <p>{job.task}</p></div>
           <div className="btn-container">
-          <div><Button className = 'bg-teal hover:bg-teal-400 text-white text-sm px-3 py-1 rounded' buttonName="Approve" onClick={handleApproveButtonClick}></Button></div>
-          <div><Button className = 'bg-teal hover:bg-teal-400 text-white text-sm px-3 py-1 rounded' buttonName="View" onClick={onViewButtonClick}></Button></div>
-          <div><Button className = 'bg-teal hover:bg-teal-400 text-white text-sm px-3 py-1 rounded' buttonName="Pause" onClick={handlePauseButtonClick}></Button></div>
-          <div><Button className = 'bg-teal hover:bg-teal-400 text-white text-sm px-3 py-1 rounded' buttonName="Revoke" onClick={handleRevokeButtonClick}></Button></div>
+            <div>
+              <Button 
+                className = 'bg-teal hover:bg-teal-400 text-white text-sm px-3 py-1 rounded' 
+                buttonName={job.status==='approved'? "Approved" : "Approve"}
+                onClick={handleApproveButtonClick}
+                disabled={job.status==='approved'}
+              />
+            </div>
+            <div>
+              <Button 
+                className = 'bg-teal hover:bg-teal-400 text-white text-sm px-3 py-1 rounded' 
+                buttonName="View" 
+                onClick={onViewButtonClick}
+              />
+            </div>
+            <div>
+              <Button 
+                className = 'bg-teal hover:bg-teal-400 text-white text-sm px-3 py-1 rounded' 
+                buttonName={job.status==='paused'? "Paused" : "Pause"}
+                onClick={handlePauseButtonClick}
+                disabled={job.status==='paused'}
+              />
+            </div>
+            <div>
+              <Button 
+                className =  {job.status==='revoked'? 'bg-red-600 ' : 'bg-teal '+'hover:bg-red-500 hover:text-white text-white text-sm px-3 py-1 rounded'}
+                buttonName={job.status==='revoked'? "Revoked" : "Revoke"}
+                onClick={handleRevokeButtonClick}
+                disabled={job.status==='revoked'}
+              />
+            </div>
           </div>
         </div>
       </div>
