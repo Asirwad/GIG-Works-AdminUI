0eae095088003ab00b54ea82faad27497a90df3c
Asirwad <asirwadsali@gmail.com>
2025-01-15 15:39:41 +0530

    added admin details

--- src/AppConfig.js
+++ src/AppConfig.js
@@ -2,4 +2,9 @@ export const applicationConfiguration = {
     api: {
         BASE_URL: "http://localhost:8089/api/v1",
     },
+    admin: {
+        id: "6757cf442a6afe01e24d0ed7",
+        name: "Alex admin",
+        email: "alex@gigworks.com",
+    }
 };
