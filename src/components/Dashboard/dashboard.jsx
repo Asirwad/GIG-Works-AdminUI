796953c6326256bac5f29cda72775c18d8df3358
Asirwad <asirwadsali@gmail.com>
2025-01-16 16:29:33 +0530

    rem unused imports

--- src/components/Dashboard/dashboard.jsx
+++ src/components/Dashboard/dashboard.jsx
@@ -1,13 +1,10 @@
-import React, { useState } from "react";
 import Header from "../headder/Header";
-import AllJobs from "../AllJobs/Alljobs";
 
 const Dashboard = () => {
 
   return (
     <div>
       <Header/>
-      <AllJobs/>
     </div>
   );
 };
